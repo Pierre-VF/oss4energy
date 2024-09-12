d532875ecbcf6dd7fb532bac59992fa015601c1a
Pierre V-F <pierre@pierrevf.consulting>
2024-09-12 09:29:58 +0000

    Add missing dependency

--- src/oss4energy/helpers.py
+++ src/oss4energy/helpers.py
@@ -5,5 +5,11 @@ Module with convenience helper functions
 import pandas as pd
 
 
-def sorted_list_of_unique_elements(x: list):
-    return list(pd.Series(x).sort_values().unique())
+def sorted_list_of_unique_elements(x: list | pd.Series):
+    if isinstance(x, list):
+        s = pd.Series(x)
+    elif isinstance(x, pd.Series):
+        s = x
+    else:
+        raise TypeError("Input must be list or pandas.Series")
+    return list(s.sort_values().unique())
