bc1b3ec49ee7f2a80b60c639a999f8ea648e5af3
Pierre V-F <pierre@pierrevf.consulting>
2024-10-01 16:15:28 +0000

    Fixing search

--- src/oss4energy/nlp/search.py
+++ src/oss4energy/nlp/search.py
@@ -26,6 +26,9 @@ class SearchResults:
 
         self.__documents = documents.copy()
 
+    def __reindex(self):
+        self.__documents = self.__documents.reset_index(drop=True)
+
     def refine_by_languages(
         self, languages: list[str], include_none: bool = False
     ) -> None:
@@ -41,6 +44,7 @@ class SearchResults:
             df_i = pd.concat([df_i, df_none])
 
         self.__documents = df_i
+        self.__reindex()
 
     def refine_by_keyword(
         self, keyword: str, description: bool = True, readme: bool = True
@@ -54,15 +58,18 @@ class SearchResults:
             k_selected = k_selected + df_i[df_i["readme"].apply(f)].index.to_list()
         k_selected_unique = list(set(k_selected))
         self.__documents = df_i.iloc[k_selected_unique].copy()
+        self.__reindex()
 
     def order_by_relevance(self, keyword: str):
         r_tfidf = tf_idf([_lower_str(i) for i in self.__documents])
         ordered_results = r_tfidf[keyword].sort_values(ascending=False)
         self.__documents = self.__documents.iloc[ordered_results.index]
+        self.__reindex()
 
     def refine_by_active_in_past_year(self):
         t_last = datetime.now(UTC) - timedelta(days=365)
         self.__documents = self.__documents[self.__documents["latest_update"] > t_last]
+        self.__reindex()
 
     @property
     def documents(self) -> pd.DataFrame:
