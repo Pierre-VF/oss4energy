5e1fc93573664957b966ff65d1f9cabedd347d8c
Pierre V-F <pierre@pierrevf.consulting>
2024-10-03 10:22:17 +0000

    Adding a mardown to plain text converter

--- src/oss4energy/nlp/markdown_io.py
+++ src/oss4energy/nlp/markdown_io.py
@@ -4,6 +4,9 @@ Module to manage markdown text
 
 import re
 
+from bs4 import BeautifulSoup
+from markdown import markdown
+
 
 def _replace_markdown_links(text):
     # Regex pattern to match markdown links
@@ -27,6 +30,20 @@ def _fix_titles_and_multiple_spaces(text: str) -> str:
 
 
 def markdown_to_clean_plaintext(x: str) -> str:
-    out = _replace_markdown_links(x)
-    out = _fix_titles_and_multiple_spaces(out)
-    return out
+    """This method converts a markdown string to plaintext
+
+    :param x: _description_
+    :return: _description_
+    """
+    html = markdown(x)
+    x = BeautifulSoup(html)
+
+    full_content = ""
+    for i in ["p", "h1", "h2", "h3"]:
+        for j in x.find_all(i):
+            full_content = full_content + " " + j.get_text(" ", strip=True)
+    # print(full_content)
+
+    # out = _replace_markdown_links(x)
+    # out = _fix_titles_and_multiple_spaces(out)
+    return full_content.strip()
